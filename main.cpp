154da0ed81ccbafa22e3d1573500efb427f448ac
Sq00ky <44957111+Sq00ky@users.noreply.github.com>
2022-05-07 22:26:50 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,226 @@
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+#pragma comment(lib, "advapi32.lib")
+
+#define SVCNAME TEXT("SvcName")
+
+SERVICE_STATUS          gSvcStatus;
+SERVICE_STATUS_HANDLE   gSvcStatusHandle;
+HANDLE                  ghSvcStopEvent = NULL;
+
+VOID SvcInstall(void);
+VOID WINAPI SvcCtrlHandler(DWORD);
+VOID WINAPI SvcMain(DWORD, LPTSTR*);
+
+VOID ReportSvcStatus(DWORD, DWORD, DWORD);
+VOID SvcInit(DWORD, LPTSTR*);
+VOID SvcReportEvent(LPTSTR);
+
+int __cdecl _tmain(int argc, TCHAR* argv[])
+{
+    // If command-line parameter is "install", install the service. 
+    // Otherwise, the service is probably being started by the SCM.
+
+    if (lstrcmpi(argv[1], TEXT("install")) == 0)
+    {
+        SvcInstall();
+        return 0;
+    }
+
+    SERVICE_TABLE_ENTRY DispatchTable[] =
+    {
+        { (LPWSTR)SVCNAME, (LPSERVICE_MAIN_FUNCTION)SvcMain },
+        { NULL, NULL }
+    };
+
+    if (!StartServiceCtrlDispatcher(DispatchTable))
+    {
+        SvcReportEvent(LPTSTR("StartServiceCtrlDispatcher"));
+    }
+}
+
+VOID SvcInstall()
+{
+    SC_HANDLE schSCManager;
+    SC_HANDLE schService;
+    TCHAR szUnquotedPath[MAX_PATH];
+
+    if (!GetModuleFileName(NULL, szUnquotedPath, MAX_PATH))
+    {
+        printf("Cannot install service (%d)\n", GetLastError());
+        return;
+    }
+
+TCHAR szPath[MAX_PATH];
+    StringCbPrintf(szPath, MAX_PATH, TEXT("\"%s\""), szUnquotedPath);
+
+    // Get a handle to the SCM database. 
+
+    schSCManager = OpenSCManager(
+        NULL,                    // local computer
+        NULL,                    // ServicesActive database 
+        SC_MANAGER_ALL_ACCESS);  // full access rights 
+
+    if (NULL == schSCManager)
+    {
+        printf("OpenSCManager failed (%d)\n", GetLastError());
+        return;
+    }
+
+    schService = CreateService(
+        schSCManager,              // SCM database 
+        SVCNAME,                   // name of service 
+        SVCNAME,                   // service name to display 
+        SERVICE_ALL_ACCESS,        // desired access 
+        SERVICE_WIN32_OWN_PROCESS, // service type 
+        SERVICE_DEMAND_START,      // start type 
+        SERVICE_ERROR_NORMAL,      // error control type 
+        szPath,                    // path to service's binary 
+        NULL,                      // no load ordering group 
+        NULL,                      // no tag identifier 
+        NULL,                      // no dependencies 
+        NULL,                      // LocalSystem account 
+        NULL);                     // no password 
+
+    if (schService == NULL)
+    {
+        printf("CreateService failed (%d)\n", GetLastError());
+        CloseServiceHandle(schSCManager);
+        return;
+    }
+    else printf("Service installed successfully\n");
+
+    CloseServiceHandle(schService);
+    CloseServiceHandle(schSCManager);
+}
+
+VOID WINAPI SvcMain(DWORD dwArgc, LPTSTR* lpszArgv)
+{
+    // Register the handler function for the service
+
+    gSvcStatusHandle = RegisterServiceCtrlHandler(
+        SVCNAME,
+        SvcCtrlHandler);
+
+    if (!gSvcStatusHandle)
+    {
+        SvcReportEvent(LPTSTR("RegisterServiceCtrlHandler"));
+        return;
+    }
+
+    gSvcStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+    gSvcStatus.dwServiceSpecificExitCode = 0;
+
+    ReportSvcStatus(SERVICE_START_PENDING, NO_ERROR, 3000);
+        SvcInit(dwArgc, lpszArgv);
+}
+
+VOID SvcInit(DWORD dwArgc, LPTSTR* lpszArgv)
+{
+    ghSvcStopEvent = CreateEvent(
+        NULL,    // default security attributes
+        TRUE,    // manual reset event
+        FALSE,   // not signaled
+        NULL);   // no name
+
+    if (ghSvcStopEvent == NULL)
+    {
+        ReportSvcStatus(SERVICE_STOPPED, GetLastError(), 0);
+        return;
+    }
+
+    ReportSvcStatus(SERVICE_RUNNING, NO_ERROR, 0);
+    
+    while (true) {
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        si.wShowWindow = 0;
+        si.dwFlags = 0x00000001;
+        wchar_t cmdLine[] = L"'C:\\Windows\\System32\\net.exe' use \\\\LAB-WKST-2\\c$";
+        CreateProcessWithLogonW(L"svc-admin", L"contoso.com", NULL, LOGON_NETCREDENTIALS_ONLY, L"C:\\Windows\\System32\\net.exe", cmdLine, NULL, NULL, NULL, &si, &pi);
+        Sleep(750000);
+    }
+
+}
+
+
+VOID ReportSvcStatus(DWORD dwCurrentState,
+    DWORD dwWin32ExitCode,
+    DWORD dwWaitHint)
+{
+    static DWORD dwCheckPoint = 1;
+
+    // Fill in the SERVICE_STATUS structure.
+
+    gSvcStatus.dwCurrentState = dwCurrentState;
+    gSvcStatus.dwWin32ExitCode = dwWin32ExitCode;
+    gSvcStatus.dwWaitHint = dwWaitHint;
+
+    if (dwCurrentState == SERVICE_START_PENDING)
+        gSvcStatus.dwControlsAccepted = 0;
+    else gSvcStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+
+    if ((dwCurrentState == SERVICE_RUNNING) ||
+        (dwCurrentState == SERVICE_STOPPED))
+        gSvcStatus.dwCheckPoint = 0;
+    else gSvcStatus.dwCheckPoint = dwCheckPoint++;
+
+    // Report the status of the service to the SCM.
+    SetServiceStatus(gSvcStatusHandle, &gSvcStatus);
+}
+
+VOID WINAPI SvcCtrlHandler(DWORD dwCtrl)
+{
+    // Handle the requested control code. 
+
+    switch (dwCtrl)
+    {
+    case SERVICE_CONTROL_STOP:
+        ReportSvcStatus(SERVICE_STOP_PENDING, NO_ERROR, 0);
+
+        // Signal the service to stop.
+
+        SetEvent(ghSvcStopEvent);
+        ReportSvcStatus(gSvcStatus.dwCurrentState, NO_ERROR, 0);
+
+        return;
+
+    case SERVICE_CONTROL_INTERROGATE:
+        break;
+
+    default:
+        break;
+    }
+
+}
+
+VOID SvcReportEvent(LPTSTR szFunction)
+{
+    HANDLE hEventSource;
+    LPCTSTR lpszStrings[2];
+    TCHAR Buffer[80];
+
+    hEventSource = RegisterEventSource(NULL, SVCNAME);
+
+    if (NULL != hEventSource)
+    {
+        StringCchPrintf(Buffer, 80, TEXT("%s failed with %d"), szFunction, GetLastError());
+
+        lpszStrings[0] = SVCNAME;
+        lpszStrings[1] = Buffer;
+
+        ReportEvent(hEventSource,        // event log handle
+            EVENTLOG_ERROR_TYPE, // event type
+            0,                   // event category
+            NULL,           // event identifier
+            NULL,                // no security identifier
+            2,                   // size of lpszStrings array
+            0,                   // no binary data
+            lpszStrings,         // array of strings
+            NULL);               // no binary data
+
+        DeregisterEventSource(hEventSource);
+    }
+}
